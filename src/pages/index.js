32618fbfb590b9149093ceb65f4262b90e703eaf
Kate Mills <kate.mills1124@gmail.com>
2020-06-09 14:26:43 -0700

    mv shell Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Img  from "gatsby-image"
-import Layout from "../components/shell"
+import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
 const Index = ({ data }) => {
